af6bb5b990ea0a7134dd3b2b2772c9eb8eb9bf08
Demian <demian@outlook.com>
2017-01-31 11:43:51 +0800

    Changed some stuff for the car

---
+++ install.sh
@@ -0,0 +1,6 @@
+apt-get install python-scipy
+pip install roshelper
+pip install shapely
+pip install planar
+mkdir third_party
+git clone https://github.com/wallarelvo/decawave_localization third_party/decawave_localization
